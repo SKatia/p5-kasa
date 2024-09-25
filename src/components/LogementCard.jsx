9f3e7e87201b5aa51cb65d288b929eb4b2dadb8f
Katerina S <e.kosheva@gmail.com>
2024-09-25 11:55:52 +0200

    nettoyage directory

--- src/components/LogementCard.jsx
+++ src/components/LogementCard.jsx
@@ -1,14 +1,14 @@
-import '../styles/logementCard.scss';
+// import '../styles/logementCard.scss';
 
-const LogementCard = ({ id, title, cover }) => {
-  return (
-    <div className="logement-card">
-      <Link to={`/logement/${id}`}>
-        <img src={cover} alt={title} className="logement-image" />
-        <h2>{title}</h2>
-      </Link>
-    </div>
-  );
-};
+// const LogementCard = ({ id, title, cover }) => {
+//   return (
+//     <div className="logement-card">
+//       <Link to={`/logement/${id}`}>
+//         <img src={cover} alt={title} className="logement-image" />
+//         <h2>{title}</h2>
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default LogementCard;
+// export default LogementCard;
