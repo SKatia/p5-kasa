2c6685a9a9940a56039d746ee2b7939572f6c694
Katerina S <e.kosheva@gmail.com>
2024-09-27 09:37:18 +0200

    Corrige home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,41 +1,7 @@
-// import { useState, useEffect } from 'react'; //
-// import '../../styles/home.css'
-// import data from './../../data/logements.json';
-
-
-// const Home = () => {
-//   const [logements, setLogements] = useState([]); 
-
-//   useEffect(() => {
-//     setLogements(data); 
-//   }, []); // useEffect без зависимостей, чтобы данные загружались один раз
-
-//   // function Home() {
-//     return (
-//       <div>
-//         <div className='banner'>
-//           <h1> Chez vous, partout et ailleurs</h1>
-//         </div>
-
-//         {/* return ( */}
-//         <div className="logements-container">
-//           {logements.map((logement) => (
-//             <div key={logement.id} className="logement-card">
-//               <img src={logement.cover} alt={logement.title} className="logement-image" />
-//               <h2>{logement.title}</h2>
-//             </div>
-//           ))}
-//         </div>
-//         {/* ); */}
-//       </div>
-//     )
-//   }
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logements from './../../data/logements.json';
 import '../../styles/home.css'
-//import logements from '../path-to-your-json/logements.json';
-//import './Home.scss'; // Подключите стили для сетки карточек
 
 const Home = () => {
   return (
